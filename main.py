0c61a18498cd1b289df3d001f91f42ccad3772bc
한수한 <hshhan1009@naver.com>
2022-11-30 20:18:21 +0900

    implement processing function

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from typing import List
+from typing import List, Dict
 import numpy as np
 from fastapi import FastAPI, Form
 import keras
 import tensorflow as tf
+from pydantic import BaseModel
+import json
+from fastapi.middleware.cors import CORSMiddleware
 # uvicorn main:app --reload
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def bbox_clip(frame_x, frame_y, axis):
     x_min = tf.reduce_min(frame_x,axis,keepdims=True)
@@ -24,8 +39,9 @@ def make_frame_to_tensor(frames):
     frame_data = np.zeros((1, num_frames, num_points*2), dtype='float32')
     for f_idx, frame in enumerate(frames):
         for i, f in enumerate(frame):
-            frame_data[1, f_idx, 2 * i] = f['x']
-            frame_data[1, f_idx, 2 * i + 1] = f['y']
+            frame_data[0, f_idx, 2 * i] = f['x']
+            frame_data[0, f_idx, 2 * i + 1] = f['y']
+    return frame_data
 
 @app.get("/")
 async def root():
@@ -36,14 +52,18 @@ exe_num = 5
 model_path = f'./trained_model/npercent_model_{exe_num}'
 model = keras.models.load_model(model_path)
 
+class Item(BaseModel):
+    frames : List[List[Dict]]
+    exe_num : int
+
 @app.post("/convert")
-async def convert(frames : List = Form(...), exe_num : int = Form(...)):
+async def convert(item:Item):
     '''모델 불러오기'''
     # model_path = f'./trained_model/npercent_model{exe_num}'
     # model = keras.models.load_model(model_path)
 
     '''bbox clip'''
-    frames = make_frame_to_tensor(frames['keypoints'])
+    frames = make_frame_to_tensor(item.frames)
     frames[:, :, ::2], frames[:, :, 1::2] = bbox_clip(frames[:, :, ::2], frames[:, :, 1::2],[1, 2])
 
     ''' trained model 에 frames 넣어 결과 도출'''
@@ -51,4 +71,6 @@ async def convert(frames : List = Form(...), exe_num : int = Form(...)):
         result = model(frames, training=False)
         result = result * 100
 
-    return result
+    print(result)
+
+    return result.cpu().numpy().tolist()[0]
